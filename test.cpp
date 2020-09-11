2b93167b90d714fe2b34989ac5e1c3cbb8a9066f
root <root@maniax-instance-1.us-central1-a.c.composed-maniax.internal>
2020-09-11 14:08:05 +0000

    테스트 커밋

---
+++ test.cpp
@@ -0,0 +1,25 @@
+//#pragma once
+#include"test.h"
+
+int main()
+{
+	try
+	{
+		cout << divideNumbers(2.5, 0.5) << endl;
+		cout << divideNumbers(2.3, 0) << endl;
+	}
+	catch(const exception& exception)
+	{
+		cout << "An exception was cought" << endl;
+	}
+
+	return 0;
+}
+
+double divideNumbers(double a, double b)
+{
+	if(b == 0)
+		throw exception();
+
+	return a / b;
+}
---
+++ test.h
@@ -0,0 +1,8 @@
+//#pragma once
+#include<iostream>
+#include<string>
+#include<stdexcept>
+
+using namespace std;
+
+double divideNumbers(double a, double b);
